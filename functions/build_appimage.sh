1fa618c49b639192ea969967cca4a23fd323f11f
Scarlett Clark <sgclark@kde.org>
2016-11-07 14:09:24 -0700

    Appimagekit again... Add an echo into build.sh to chown jenkins to the files written to /out... for some baffeling reason they are owned by scarlett user, which is shocking as jenkins is running the script.

--- functions/build_appimage.sh
+++ functions/build_appimage.sh
@@ -3,5 +3,5 @@ sed -i -e 's|archive3.h|archive.h|g' ./shared.c
 sed -i -e 's|archive_entry3.h|archive_entry.h|g' ./shared.c
 sed -i -e 's|-larchive3|-larchive|g' ./build.sh
 sed -i -e 's|mkdir|#mkdir|g' ./build.sh
-
+echo "sudo chown jenkins.jenkins out/*" >> ./build.sh
 ./build.sh
