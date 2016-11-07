cda4e92d74aa8e416873fbebe40e7f032c7f9346
Scarlett Clark <sgclark@kde.org>
2016-11-07 07:07:12 -0700

    Comment out all mkdir in appimagekit build script. If I want something moved somewhere I will do it myself, as this is wreaking havoc on premounted directories.

--- functions/build_appimage.sh
+++ functions/build_appimage.sh
@@ -2,6 +2,6 @@
 sed -i -e 's|archive3.h|archive.h|g' ./shared.c
 sed -i -e 's|archive_entry3.h|archive_entry.h|g' ./shared.c
 sed -i -e 's|-larchive3|-larchive|g' ./build.sh
-sed -i -e 's|mkdir -p /out/|#mkdir -p /out/|g' ./build.sh
+sed -i -e 's|mkdir|#mkdir|g' ./build.sh
 
 ./build.sh
