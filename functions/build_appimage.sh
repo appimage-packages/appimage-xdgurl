6a401c1b66366f653db29457ef912ee07423eb07
Scarlett Clark <sgclark@kde.org>
2016-11-08 06:35:51 -0700

    Fighting with out directory again, we are not running as root for good reason, but script assumes we are.

--- functions/build_appimage.sh
+++ functions/build_appimage.sh
@@ -2,5 +2,5 @@
 sed -i -e 's|archive3.h|archive.h|g' ./shared.c
 sed -i -e 's|archive_entry3.h|archive_entry.h|g' ./shared.c
 sed -i -e 's|-larchive3|-larchive|g' ./build.sh
-
+sed -i -e 's|//out//|//app//out//|g' ./build.sh
 ./build.sh
