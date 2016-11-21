8ce514f4aa42c62e7b14c8ee93dc4aabcd0033f1
Scarlett Clark <sgclark@kde.org>
2016-11-21 07:38:28 -0700

    chown new directories

--- setup.sh
+++ setup.sh
@@ -20,4 +20,7 @@
 export PATH=/opt/usr/bin:/home/jenkins/.rbenv/bin:/home/jenkins/.rbenv/shims:$PATH
 export WORKSPACE=`pwd`
 echo $WORKSPACE
+sudo chown jenkins.jenkins '/app'
+sudo chown jenkins.jenkins '/appimage'
+sudo chown jenkins.jenkins '/out'
 cd /in && bundle install && rspec spec/recipe_rspec.rb --fail-fast
