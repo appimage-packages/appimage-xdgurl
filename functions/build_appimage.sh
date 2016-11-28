2166de5ce712b3ccf36e338866180c51d958d275
Scarlett Clark <sgclark@kde.org>
2016-11-28 03:49:17 -0700

    hange working dir for appimage creation. Add verbosity.

--- functions/build_appimage.sh
+++ functions/build_appimage.sh
@@ -2,10 +2,11 @@
 
 # Run linuxdeployqt to bring in proper libs
 find $HOME/build-*-*_Qt_* \( -name "moc_*" -or -name "*.o" -or -name "qrc_*" -or -name "Makefile*" -or -name "*.a" \) -exec rm {} \;
+cd /
 mkdir /xdgurl.AppDir/
 mv /app/usr /xdgurl.AppDir/usr
-cp   /xdgurl.AppDir/usr/share/applications/xdgurl.desktop .
-cp  /xdgurl.AppDir/usr/share/icons/hicolor/scalable/apps/xdgurl.svg .
+cp  -v  /xdgurl.AppDir/usr/share/applications/xdgurl.desktop .
+cp  -v /xdgurl.AppDir/usr/share/icons/hicolor/scalable/apps/xdgurl.svg .
 wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage
 chmod a+x linuxdeployqt-1-x86_64.AppImage
 
